a689c3de738a20cc55ff09cda4591f36ec56a57d
jhao7 <>
2019-05-17 11:40:46 +1000

    first commit
    create a new MVC project with authentification

---
+++ CloudProgrammingTask6/CloudProgrammingTask6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudProgrammingTask6.MvcApplication" Language="C#" %>
---
+++ CloudProgrammingTask6/CloudProgrammingTask6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudProgrammingTask6.Startup))]
+namespace CloudProgrammingTask6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
