4f4c77b5c9eb0876acc995b86104f55b885f7af2
jhao7 <>
2019-05-17 12:15:32 +1000

    second commit
    add a new data model class
    create corresponding controller and views
    set authorization for those views

---
+++ CloudProgrammingTask6/CloudProgrammingTask6/Controllers/BookingRecordsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CloudProgrammingTask6.Models;
+
+namespace CloudProgrammingTask6.Controllers
+{
+    public class BookingRecordsController : Controller
+    {
+        private ContactDBContext db = new ContactDBContext();
+
+        [Authorize]
+        // GET: BookingRecords
+        public ActionResult Index()
+        {
+            return View(db.Bookings.ToList());
+        }
+
+        // GET: BookingRecords/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingRecord bookingRecord = db.Bookings.Find(id);
+            if (bookingRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingRecord);
+        }
+
+        // GET: BookingRecords/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookingRecords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,Age,Gender,Departure,Arrival,DepartureTime,ArrivalTime,FlightNumber,Price,FareClass,PromoCode,State,Note")] BookingRecord bookingRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bookings.Add(bookingRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bookingRecord);
+        }
+
+        // GET: BookingRecords/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingRecord bookingRecord = db.Bookings.Find(id);
+            if (bookingRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingRecord);
+        }
+
+        // POST: BookingRecords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Age,Gender,Departure,Arrival,DepartureTime,ArrivalTime,FlightNumber,Price,FareClass,PromoCode,State,Note")] BookingRecord bookingRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookingRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bookingRecord);
+        }
+
+        // GET: BookingRecords/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingRecord bookingRecord = db.Bookings.Find(id);
+            if (bookingRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingRecord);
+        }
+
+        // POST: BookingRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BookingRecord bookingRecord = db.Bookings.Find(id);
+            db.Bookings.Remove(bookingRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CloudProgrammingTask6/CloudProgrammingTask6/Models/BookingRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CloudProgrammingTask6.Models
+{
+    public class BookingRecord
+    {
+        public int ID { get; set; }
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        public String Gender { get; set; }
+        public String Departure { get; set; }
+        public String Arrival { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public String FlightNumber { get; set; }
+        public Double Price { get; set; }
+        public String FareClass { get; set; }
+        public String PromoCode { get; set; }
+        public String State { get; set; }
+        public String Note { get; set; }
+
+    }
+
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<BookingRecord> Bookings { get; set; }
+    }
+}
