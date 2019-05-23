071cf866294afcd44c6e43d677dfc0f8b7fc985b
jhao7 <>
2019-05-23 16:26:02 +1000

    4th commit

--- CloudProgrammingTask6/CloudProgrammingTask6/Controllers/HomeController.cs
+++ CloudProgrammingTask6/CloudProgrammingTask6/Controllers/HomeController.cs
@@ -6,13 +6,31 @@ using System.Web.Mvc;
 
 namespace CloudProgrammingTask6.Controllers
 {
+
+    /// <summary>
+    /// The controller for the home page.
+    /// </summary>
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// The function for displaying the home page
+        /// </summary>
+        /// /// <returns>
+        /// Returns a view of home page.
+        /// </returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// The function for displaying the about page
+        /// </summary>
+        /// /// <returns>
+        /// <para>Returns a view of about page.</para>
+        /// <para>Returns a floating message.</para>
+        /// </returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +38,13 @@ namespace CloudProgrammingTask6.Controllers
             return View();
         }
 
+        /// <summary>
+        /// The function for displaying the contact page
+        /// </summary>
+        /// /// <returns>
+        /// <para>Returns a view of contact page.</para>
+        /// <para>Returns a floating message.</para>
+        /// </returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
